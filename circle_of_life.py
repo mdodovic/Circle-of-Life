2c79ae999d92f22b03fe6160ef40fc56ab577e98
mdodovic <matija.dodovic@gmail.com>
2022-02-22 17:21:34 +0100

    add equal ratio

--- circle_of_life.py
+++ circle_of_life.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from automate import *
 
 SAME_VALUE = 0
+SAME_RATIO = 1
 
 
 def initialization():
@@ -51,7 +52,7 @@ def run_single_circle_of_life(start_A, start_L, start_t, end_t, delta_t, p, q, r
     return array_t, array_A, array_L
 
 
-def equal_number_check(array_antelope, array_lion, epsilon):
+def equal_number_check(array_time, array_antelope, array_lion, epsilon):
 
     for i in range(1, len(array_antelope)):
         if np.abs(array_antelope[i] - array_antelope[i - 1]) > epsilon:
@@ -61,28 +62,49 @@ def equal_number_check(array_antelope, array_lion, epsilon):
         if np.abs(array_lion[i] - array_lion[i - 1]) > epsilon:
             return False
 
+    plt.plot(array_time, array_antelope)
+    plt.plot(array_time, array_lion)
+    plt.show()
+
     return True
 
 
-def check_animal_number(check_type, array_antelope, array_lion, epsilon):
+def equal_ratio_check(array_time, array_antelope, array_lion, epsilon):
+
+    array_ratio = array_lion / array_antelope
+    #print(array_antelope, array_lion)
+
+    for i in range(1, len(array_ratio)):
+        if np.abs(array_ratio[i] - array_ratio[i - 1]) > epsilon:
+            return False
+
+    if (max(array_ratio) - min(array_ratio)) > epsilon:
+        return False
+
+    plt.plot(array_time, array_ratio)
+    plt.show()
+
+    return True
+
+
+def check_animal_number(check_type, array_time, array_antelope, array_lion, epsilon):
 
     if check_type == SAME_VALUE:
-        return equal_number_check(array_antelope, array_lion, epsilon)
+        return equal_number_check(array_time, array_antelope, array_lion, epsilon)
+    elif check_type == SAME_RATIO:
+        return equal_ratio_check(array_time, array_antelope, array_lion, epsilon)
 
 
 def main():
     decimal_places, epsilon, delta, A, L, start_time, delta_time, end_time, p = initialization()
+    """
+    q = round(5.660377358490566, decimal_places)
+    r = round(5.357142857142855, decimal_places)
+    array_t, array_A, array_L = run_single_circle_of_life(A, L, start_time, end_time, delta_time, p, q, r)
 
-    # q = round(5.660377358490566, decimal_places)
-    # r = round(5.357142857142855, decimal_places)
-    # array_t, array_A, array_L = run_single_circle_of_life(A, L, start_time, end_time, delta_time, p, q, r)
-    #
-    # if check_animal_number(SAME_VALUE, array_A, array_L, epsilon):
-    #     print(q, r)
-    #     plt.plot(array_t, array_A)
-    #     plt.plot(array_t, array_L)
-    #     plt.show()
-
+    if check_animal_number(SAME_RATIO, array_t, array_A, array_L, epsilon):
+        print(q, r)
+    """
     q = delta
     while q < 100:
 
@@ -92,11 +114,7 @@ def main():
             # print(q, r)
             array_t, array_A, array_L = run_single_circle_of_life(A, L, start_time, end_time, delta_time, p, q, r)
 
-            if check_animal_number(SAME_VALUE, array_A, array_L, epsilon):
-                plt.plot(array_t, array_A)
-                plt.plot(array_t, array_L)
-                plt.show()
-
+            if check_animal_number(SAME_RATIO, array_t, array_A, array_L, epsilon):
                 return p, q, r
 
             r = round(r + delta, decimal_places)
